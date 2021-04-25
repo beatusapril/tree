74f809d6c0ad748916d4b3d93cd6d53ea7196c5f
beatusapril <zvoischeva@gmail.com>
2021-04-25 23:01:10 +0300

    added some refactoring

--- src/main/webapp/js/tree.js
+++ src/main/webapp/js/tree.js
@@ -12,11 +12,11 @@ Object.defineProperty(Array.prototype, "toTree", {
     configurable: false,
     writable: false,
     value: function (getKey, getParentKey) {
-        var list = JSON.parse(JSON.stringify(this));
-        var root = {};
-        for (var index = 0; index < list.length; index++) {
-            var parentKey = getParentKey.call(list, list[index], index, list);
-            var parent = (list.find(function (item, index) {
+        let list = JSON.parse(JSON.stringify(this));
+        let root = {};
+        for (let index = 0; index < list.length; index++) {
+            let parentKey = getParentKey.call(list, list[index], index, list);
+            let parent = (list.find(function (item, index) {
                 return parentKey === getKey.call(list, item, index, list);
             }) || root);
             (parent.children = parent.children || []).push(list[index]);
@@ -28,14 +28,13 @@ Object.defineProperty(Array.prototype, "toTree", {
 function createTreeView(tree, getName) {
     let listDom = document.createElement("ul");
     listDom.className = "treeline";
-    console.log("start")
     tree.forEach(function (item) {
         let itemDom = listDom.appendChild(document.createElement("li"));
         if (item.children && item.children.length) {
-            var itemName = itemDom.appendChild(document.createElement("span"));
+            let itemName = itemDom.appendChild(document.createElement("span"));
             itemName.textContent = getName.call(item, item);
             itemName.className = "caret";
-            var nestedList = itemDom.appendChild(createTreeView(item.children, getName));
+            let nestedList = itemDom.appendChild(createTreeView(item.children, getName));
             nestedList.className = "nested";
         } else {
             itemDom.textContent = getName.call(item, item);
@@ -44,12 +43,12 @@ function createTreeView(tree, getName) {
     return listDom;
 }
 
-function addNestedMechanism(){
+function addNestedMechanism() {
     let toggler = document.getElementsByClassName("caret");
     let i;
 
     for (i = 0; i < toggler.length; i++) {
-        toggler[i].addEventListener("click", function() {
+        toggler[i].addEventListener("click", function () {
             this.parentElement.querySelector(".nested").classList.toggle("active");
             this.classList.toggle("caret-down");
         });
