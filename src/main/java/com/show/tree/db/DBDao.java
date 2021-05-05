f52b77aa1821f55539b25efffeb5fd5cfe481172
beatusapril <zvoischeva@gmail.com>
2021-05-05 20:43:23 +0300

    Adding create, update operation

--- src/main/java/com/show/tree/db/DBDao.java
+++ src/main/java/com/show/tree/db/DBDao.java
@@ -2,10 +2,7 @@ package com.show.tree.db;
 
 import com.show.tree.domain.File;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -32,6 +29,43 @@ public class DBDao {
             System.out.println(e.getMessage());
             return Collections.emptyList();
         }
+    }
+
+    public void create(File file) {
+        try {
+            String sql = "INSERT INTO (record_id, parent_id, name) VALUES (?, ?, ?)";
+            PreparedStatement statement = getPreparedStatement(file, sql);
+            statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(File file, String sql) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, file.getId());
+        statement.setInt(2, file.getParentId());
+        statement.setString(3, file.getName());
+        return statement;
+    }
+
+    public void update(File file) {
+        try {
+            String sql = "UPDATE files (record_id, parent_id, name) VALUES (?, ?, ?)";
+            PreparedStatement statement = getPreparedStatement(file, sql);
+            statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void delete(Integer parentId, String name) throws SQLException {
+        String sql = "DELETE FROM files WHERE parent_id = ? AND name = ? ";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, parentId);
+        statement.setString(2, name);
+        statement.executeQuery(sql);
     }
 
     private List<File> mapFiles(ResultSet resultSet) throws SQLException {
--- src/main/java/com/show/tree/service/DataService.java
+++ src/main/java/com/show/tree/service/DataService.java
@@ -3,6 +3,7 @@ package com.show.tree.service;
 import com.show.tree.db.DBDao;
 import com.show.tree.domain.File;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class DataService {
@@ -15,5 +16,13 @@ public class DataService {
 
     public List<File> getAllFiles() {
         return dbDao.getAllFiles();
+    }
+
+    public void create(File file) {
+        dbDao.create(file);
+    }
+
+    public void delete(File file) throws SQLException {
+        dbDao.delete(file.getParentId(), file.getName());
     }
 }
